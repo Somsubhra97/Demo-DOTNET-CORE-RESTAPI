9b3d9d1316a23e76fdad051265fe92e2f58ed9d6
Somsubhra97 <somsubhrablasterjaxxmitra@gmail.com>
2020-10-05 14:24:39 +0530

    second commit

---
+++ Dtos/Command/AddCommandDto.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace Project.Dtos.Command
+{
+    public class AddCommandDto
+    {
+        public int Id { get; set; }
+        
+        public string HowTo { get; set; }
+        
+        public string Line { get; set; }
+        
+    }
+}
